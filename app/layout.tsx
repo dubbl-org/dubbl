b200922dbbdb00f4c411a4e956e7b314acf7406a
Matthew Meszaros <meszmatew@gmail.com>
2026-03-02 06:40:21 +0000

    feature: suppress theme hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
       >
